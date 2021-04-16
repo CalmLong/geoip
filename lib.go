cca1710f1f21d8b4da3985a967f57f672aa692c4
CalmLong <37164399+CalmLong@users.noreply.github.com>
2021-04-16 16:29:21 +0800

    增加 Google IP

--- lib.go
+++ lib.go
@@ -4,8 +4,11 @@ const countryCN = "CN"
 
 const (
 	ipV41 = "https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china.txt"
+	ipV42 = "https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/googlecn.txt"
+	
 	ipV61 = "https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china6.txt"
+	ipV62 = "https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/googlecn6.txt"
 )
 
-var ipv4s = []string{ipV41}
-var ipv6s = []string{ipV61}
+var ipv4s = []string{ipV41, ipV42}
+var ipv6s = []string{ipV61, ipV62}
